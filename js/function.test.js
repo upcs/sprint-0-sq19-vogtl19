e6d7bd57b5d289f79c052d14cae4eeb95d77cb5f
Langley <vogtl19@up.edu>
2019-01-28 21:53:28 -0800

    Update function.test.js
    Added unit test that is supposed to fail

--- js/function.test.js
+++ js/function.test.js
@@ -6,9 +6,13 @@
 const addition = require('./function');
 
 test('Should add 1 + 2 to equal 3', () => {
-  expect(addition(1, 2)).toBe(3);
+	expect(addition(1, 2)).toBe(3);
 });
 
 test('Should add two negative numbers, -1 and -2 to get -3', () => {
-  expect(addition(-1, -2)).toBe(-3);
+	expect(addition(-1, -2)).toBe(-3);
+});
+
+test('Should fail', () => {
+	expect(addition(5,1).toBe(0);
 });
